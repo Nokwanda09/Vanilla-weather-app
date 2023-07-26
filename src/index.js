88c6e516343139c687c092f412852191e80d6529
Nokwanda <mpungosesingabenksi@gmail.com>
2023-07-26 12:40:33 +0200

    Updated date and time

--- src/index.js
+++ src/index.js
@@ -2,18 +2,42 @@ let apiKey = "8123783b747891cdafd883d799046f7c";
 let city = "Durban";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    `0${hours}`;
+  }
+  let munites = date.getMinutes();
+  if (munites < 10) {
+    `0${munites}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${munites}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let despcription = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   despcription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   windSpeed.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(apiUrl).then(displayTemperature);
