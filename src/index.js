484cb907ad6bf4b1137ccf6386c02d9d24a75ba0
Nokwanda <mpungosesingabenksi@gmail.com>
2023-07-27 11:39:06 +0200

    Added relevant icon from API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let apiKey = "8123783b747891cdafd883d799046f7c";
-let city = "Durban";
+let city = "Cape Town";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 function formatDate(timestamp) {
@@ -26,18 +26,26 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let despcription = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let icon = response.data.weather[0].icon;
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   despcription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   windSpeed.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${icon}@2x.png`
+  );
 }
 
 axios.get(apiUrl).then(displayTemperature);
