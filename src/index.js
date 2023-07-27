8e9b37386b11d0f3eac2f98f030669fd8b11d34d
Nokwanda <mpungosesingabenksi@gmail.com>
2023-07-27 12:35:15 +0200

    Added search engine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-let apiKey = "8123783b747891cdafd883d799046f7c";
-let city = "Durban";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -47,4 +43,19 @@ function displayTemperature(response) {
   );
 }
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "8123783b747891cdafd883d799046f7c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
